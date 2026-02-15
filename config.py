fddb8c7dc06dfb68c6b7123da9899d923d6bd450
nagadeepch2233 <nagadeepch@gmail.com>
2026-02-15 15:34:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import json
 
-API_KEY = "f8a559765b3c778b654acfd85645d644"  
+API_KEY = "Type your API Key here"  
 BASE_URL = "https://api.openweathermap.org/data/2.5"
 CACHE_DIR = Path("weather_cache")
 CACHE_DIR.mkdir(exist_ok=True)
